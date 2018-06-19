1c0e84572f699bbf1f0acfb5016d3c098accc04a
zhasav-bbc <Vion.Hasaj@bbc.ch>
2018-06-19 11:19:53 +0200

    connection for database

--- CoolShoes/src/DBTest.java
+++ CoolShoes/src/DBTest.java
@@ -9,7 +9,7 @@ public class DBTest {
 	private String path = "CoolShoes.accdb";
 	private Connection conn;
 
-	{
+	public void connect() {
 		try {
 			Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
 			conn = DriverManager.getConnection("jdbc:ucanaccess://" + path);
@@ -20,17 +20,18 @@ public class DBTest {
 		}
 
 	}
-	{
+
+	public void read() {
 		try {
 			// Eine Abfrage an die Datenbank richten
 			Statement stmt = conn.createStatement();
-			ResultSet rs = stmt.executeQuery("SELECT * FROM Kunde;");
+			ResultSet rs = stmt.executeQuery("SELECT * FROM Mitarbeiter;");
 			// die Anzahl der Einträge des Datensatzes lesen
 			int column = rs.getMetaData().getColumnCount();
 			// Datensatz umd Datensatz abarbeiten
 			while (rs.next()) {
 				for (int i = 1; i <= column; i++) // Beginnt mit Position 1 !!!
-					rs.getString(i); // hier erfolgt die Verarbeitung der Daten
+					System.out.print(rs.getString(i)); // hier erfolgt die Verarbeitung der Daten
 			}
 			rs.close(); // nach Gebrauch sind des ResultSet...
 			stmt.close(); // ...und das Statement zu schliessen.
@@ -38,7 +39,8 @@ public class DBTest {
 			System.out.println("ungültiger SQL-Befehl");
 		}
 	}
-	{
+
+	public void write() {
 		try {
 			// Einen insert an die Datenbank richten
 			Statement stmt = conn.createStatement();
@@ -47,6 +49,13 @@ public class DBTest {
 		} catch (SQLException err) {
 			System.out.println("ungültiger SQL-Befehl");
 		}
+
+	}
+
+	public static void main(String[] args) {
+		DBTest dbt = new DBTest();
+		dbt.connect();
+		dbt.read();
 
 	}
 }
