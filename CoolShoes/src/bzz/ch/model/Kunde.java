ed131f8e7bbb62fa66ec758e15517015a1ba3920
zhasav-bbc <Vion.Hasaj@bbc.ch>
2018-07-02 20:24:19 +0200

    enhanced the class by adding needed variables

--- CoolShoes/src/bzz/ch/model/Kunde.java
+++ CoolShoes/src/bzz/ch/model/Kunde.java
@@ -2,9 +2,78 @@ package bzz.ch.model;
 
 public class Kunde {
 	
-	private String KName;
-	private String KVorname;
-	private String KAdresse;
-	private int KPLZ;
+	private int id;
+	private String name;
+	private String vorname;
+	private String adresse;
+	private int plz;
+	private String ort;
+	private String eMail;
+	private String passwort;
+	
+	public Kunde(int id, String name, String vorname, String adresse, int plz, String ort, String eMail, String passwort) {
+		this.id = id;
+		this.name = name;
+		this.vorname = vorname;
+		this.adresse = adresse;
+		this.plz = plz;
+		this.ort = ort;
+		this.eMail = eMail;
+		this.passwort = passwort;
+	}
+	
+	public int  getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getVorname() {
+		return vorname;
+	}
+	public void setVorname(String vorname) {
+		this.vorname = vorname;
+	}
+	public String getAdresse() {
+		return adresse;
+	}
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+	public int getPlz() {
+		return plz;
+	}
+	public void setPlz(int plz) {
+		this.plz = plz;
+	}
+	public String getOrt() {
+		return ort;
+	}
+	public void setOrt(String ort) {
+		this.ort = ort;
+	}
+	public String geteMail() {
+		return eMail;
+	}
+	public void seteMail(String eMail) {
+		this.eMail = eMail;
+	}
+	public String getPasswort() {
+		return passwort;
+	}
+	public void setPasswort(String passwort) {
+		this.passwort = passwort;
+	}
+
+	@Override
+	public String toString() {
+		return vorname + " " + name;
+	}
 	
 }
--- CoolShoes/src/bzz/ch/model/Mitarbeiter.java
+++ CoolShoes/src/bzz/ch/model/Mitarbeiter.java
@@ -2,19 +2,37 @@ package bzz.ch.model;
 
 public class Mitarbeiter {
 	
-	private Bestellung bestellung;
-	private String MAName;
-	private String MAVorname;
+	private int id;
+	private String name;
+	private String vorname;
 	
-	public void auftragTeilen(String bestellnummeer, String status) {
-		
+	public Mitarbeiter(int id, String name, String vorname) {
+		this.id = id;
+		this.name = name;
+		this.vorname = vorname;
 	}
 	
-	public void statusAendern(Bestellung bestellung) {
-		
+	public int getId() {
+		return id;
 	}
-	
-	public void showHistory() {
-		
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getVorname() {
+		return vorname;
+	}
+	public void setVorname(String vorname) {
+		this.vorname = vorname;
+	}
+
+	@Override
+	public String toString() {
+		return vorname + " " + name;
 	}
 }
